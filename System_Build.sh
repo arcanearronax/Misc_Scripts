e44f0d4a438be14eacc8daa94d54c8672b0b516e
arcanearronax <arcanearronax@aol.com>
2017-08-29 01:48:43 +0000

    Working on making a universal setup script. At least universal for my needs.

--- System_Build.sh
+++ System_Build.sh
@@ -1,31 +1,49 @@
-#!/bin/bash
-
-#   Raspberry Pi Build Script
-#   arronax - 08/19/2017
+#   System Build Script
+#   arronax - 08/27/2017
 #   
 #   Must run without sudo.
 
 #   Uncomment most everything before running
+#   Running as ./System_Build -y will bypass continue prompts
 
-#Get our user's home directory
-USR_DIR=`echo ~/`
-echo $USR_DIR
+# Get our arg vars set
+for i in $@; do
+    FAST=0
+    case $i in
+        -y)
+            FAST=1
+            ;;
+        *)
+            echo "ARG: $i is unknown. Exiting Script."
+            exit 1
+            ;;
+    esac
+done
+
+echo $1
+
+# Get the user's base information
+USR_DIR=$HOME
+#   Running as ./System_Build -y will bypass continue prompts
+
+
+USR_NAM=$USER
+echo "Welcome $USR_NAM, your home is $USR_DIR."
 cd $USR_DIR
-
-# Welcome
-echo "Welcome $USER."
-SCR_USR=$USER
-echo "Your Home Directory is $USR_DIR."
 echo "Running config script..."
-#sleep 1
+if [ "$FAST" != 1 ]
+then
+    sleep 1
+    echo "TEST PRINT"
+fi
 
 # Need to enable SSH, user must do this for now
-echo "User must enable SSH in GUI..."
+#echo "User must enable SSH in GUI..."
 #sleep 2
 #sudo raspi-config
 
 # Update stuff
-echo "Update package manager..."
+#echo "Update package manager..."
 #sleep 2
 #sudo apt-get update -y
 #sudo apt-get upgrade -y
@@ -43,22 +61,22 @@ echo "Update package manager..."
 #echo 'set expandtab           " Expand \t to spaces' >> $VIM_RC
 
 # Get Git configured
-echo "Install and configure Git..."
+#echo "Install and configure Git..."
 #sleep 2
 #sudo apt-get install git -y
 
 #read -p "Please enter you Git username. Simply press Enter to skip this. " GIT_USR
 #git config --global user.name "$GIT_USR"
-echo "Username set: "
-git config --global user.name
+#echo "Username set: "
+#git config --global user.name
 
 #read -p "Please enter your Git email. Simply press Enter to skip this. " GIT_EMA
 #git config --global user.email "$GIT_EMA"
-echo "Email set: "
-git config --global user.email
+#echo "Email set: "
+#git config --global user.email
 
 # Enable key auth, assuming it's in place
-echo "Enable SSH Key Auth for Git..."
+#echo "Enable SSH Key Auth for Git..."
 #sleep 2
 #SSH_CONF=$USR_DIR'.ssh/config'
 #echo 'host github.com' >> $SSH_CONF
@@ -66,11 +84,11 @@ echo "Enable SSH Key Auth for Git..."
 #echo '  IdentityFile ~/.ssh/id_rsa' >> $SSH_CONF
 #echo '  User git' >> $SSH_CONF
 
-echo "Remember to clone your repos."
+#echo "Remember to clone your repos."
 #sleep 2
 
 # I like tree now that I know about it
-echo "Installing Tree..."
+#echo "Installing Tree..."
 #sleep 2
 #sudo apt-get install tree -y
 
@@ -78,15 +96,15 @@ echo "Installing Tree..."
 #sudo apt-get install mysql-server python-mysqldb mysql-client -y
 # Need to set root password
 #sleep 2
-SQL_PID=`sudo cat /var/run/mysqld/mysqld.pid`
+#SQL_PID=`sudo cat /var/run/mysqld/mysqld.pid`
 #echo "P_ID: $SQL_PID"
 #sudo kill $SQL_PID
 #echo "Create temp_conf here:"
 #pwd
-INIT_F=$USR_DIR'temp_conf.sql'
+#INIT_F=$USR_DIR'temp_conf.sql'
 #echo "alter user 'root'@'localhost' identified by 'Chance1791';" >> $INIT_F
 #echo "INIT_F = $INIT_F"
-SU_SCR=$USR_DIR'.temp.sh'
+#SU_SCR=$USR_DIR'.temp.sh'
 #echo "mysqld --init-file=$INIT_F &" >> $SU_SCR
 #sudo rm $INIT_F
 #sudo chmod u+x $SU_SCR
@@ -98,10 +116,10 @@ SU_SCR=$USR_DIR'.temp.sh'
 # By this point root will be able to login to mysql
 
 # Need to add a user for the person running the script
-SQL_CNF=$USR_DIR'.sql_conf.sql'
+#SQL_CNF=$USR_DIR'.sql_conf.sql'
 #read -p "Enter a password for your MySQL localhost account: " SQL_PASS
 #echo "grant all privileges on test.* to '$SCR_USR'@'localhost' identified by '$SQL_PASS';" >> $SQL_CNF
-USR_DB="${SCR_USR}_database"
+#USR_DB="${SCR_USR}_database"
 #echo "create database $USR_DB;" >> $SQL_CNF
 #echo "grant all privileges on $USR_DB.* to '$SCR_USR'@'localhost' identified by '$SQL_PASS';" >> $SQL_CNF
 #sudo mysql < $SQL_CNF
