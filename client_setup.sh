ae8d3d58b0e730d2f710259f189d7c42571b68c9
arcanearronax <arronax@aol.com>
2019-01-03 23:06:48 -0500

    Quick script to set up client side files to build the website.

--- client_setup.sh
+++ client_setup.sh
@@ -1,1 +1,18 @@
-#!/usr/bin/bash
+#/usr/bin/bash
+
+# Get the local website files set up
+mkdir ~/website
+cd ~/website
+
+SHIP="client_ship.sh"
+PI="10.0.0.5"
+WEBDIR="/home/webmaster/web_proj"
+BLOG="$WEBDIR/blog"
+STATIC="$WEBDIR/static"
+scp -r arronax@$PI:$BLOG ./
+scp -r arronax@$PI:$STATIC ./
+
+echo "scp -r ./blog arronax@$PI:$WEBDIR" > $SHIP
+echo "scp -r ./static arronax@$PI:$WEBDIR" > $SHIP
+
+chmod u+x $SHIP
